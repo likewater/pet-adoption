c4c66030eb71d3ae2f25e4085174adac5c992b7b
Blake Womak and Grace Stuart <blake@>
2017-03-06 12:10:09 -0800

    add comment in js file about future work.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//back end logic
+
+//front end logic
+$(document).ready(function() {
+  $("btn#seeAdopt").click(function() {
+    $("#").hide(); // will call all objects with the "adopted" property value.
+  });
+});
